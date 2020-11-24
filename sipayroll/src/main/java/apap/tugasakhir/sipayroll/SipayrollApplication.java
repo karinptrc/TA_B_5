ef7dd386a0cbb93aba2734babbe0cc4d2eed063a
Syanne Limarwan <syannelimarwan@gmail.com>
2020-11-24 19:30:23 +0700

    initializing project

---
+++ sipayroll/src/main/java/apap/tugasakhir/sipayroll/SipayrollApplication.java
@@ -0,0 +1,13 @@
+package apap.tugasakhir.sipayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SipayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SipayrollApplication.class, args);
+	}
+
+}
---
+++ sipayroll/src/test/java/apap/tugasakhir/sipayroll/SipayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.tugasakhir.sipayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SipayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

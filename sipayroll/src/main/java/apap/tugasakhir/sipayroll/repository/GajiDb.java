1fada92dd85f2d6c23d2984c5c096b2dafd67683
karinptrc <karin.patricia@ui.ac.id>
2020-11-27 21:53:47 +0700

    refactor: sipayroll - add repository gaji

---
+++ sipayroll/src/main/java/apap/tugasakhir/sipayroll/repository/GajiDb.java
@@ -0,0 +1,9 @@
+package apap.tugasakhir.sipayroll.repository;
+
+import apap.tugasakhir.sipayroll.model.GajiModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GajiDb extends JpaRepository<GajiModel, Long> {
+}

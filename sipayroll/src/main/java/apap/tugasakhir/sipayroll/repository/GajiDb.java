41cbf2e48d8066734ddd839076d1f7f173e20b81
karinptrc <karin.patricia@ui.ac.id>
2020-11-29 16:59:27 +0700

    fix: sipayroll - fix conflict in repository gaji

--- sipayroll/src/main/java/apap/tugasakhir/sipayroll/repository/GajiDb.java
+++ sipayroll/src/main/java/apap/tugasakhir/sipayroll/repository/GajiDb.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface GajiDb extends JpaRepository<GajiModel, Long> {
+    Optional<GajiModel> findById(Integer id);
     List<GajiModel> findByTanggalMasukBefore(LocalDate time);
 }

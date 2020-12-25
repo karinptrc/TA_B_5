433031ee7e8801a274bb344bba0d10098216254a
hanna_jtm <hanna14jtm@gmail.com>
2020-12-25 11:42:52 +0700

    fix: add transactional annotation at user model

--- sipayroll/src/main/java/apap/tugasakhir/sipayroll/model/UserModel.java
+++ sipayroll/src/main/java/apap/tugasakhir/sipayroll/model/UserModel.java
@@ -6,12 +6,14 @@ import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
+import javax.transaction.Transactional;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.List;
 
 @Entity
+@Transactional
 @Table(name="user_model")
 public class UserModel implements Serializable {
     @Id

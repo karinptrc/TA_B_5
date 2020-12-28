449b1e6afdeb9f38e4f80456f56a50c5df4f23c8
Syanne Limarwan <syannelimarwan@gmail.com>
2020-12-28 20:13:20 +0700

    fixing minor conditions in lembur and adding link to sirekrutment

--- sipayroll/src/main/java/apap/tugasakhir/sipayroll/rest/Setting.java
+++ sipayroll/src/main/java/apap/tugasakhir/sipayroll/rest/Setting.java
@@ -2,5 +2,5 @@ package apap.tugasakhir.sipayroll.rest;
 
 public class Setting {
     final public static String pegawaiAPI = "https://si-pegawai.herokuapp.com/api/v1";
-    final public static String lowonganAPI = "https://731e12d6-d02d-473a-9c13-bb8db9809bf7.mock.pstmn.io";
+    final public static String lowonganAPI = "https://apap-sirektumen.herokuapp.com/api/v1";
 }

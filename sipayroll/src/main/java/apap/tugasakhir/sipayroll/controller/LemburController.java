6a2f32605c045bdd2e328626440aa35a046274a4
Syanne Limarwan <syannelimarwan@gmail.com>
2020-12-28 20:32:25 +0700

    fixing fitur 11

--- sipayroll/src/main/java/apap/tugasakhir/sipayroll/controller/LemburController.java
+++ sipayroll/src/main/java/apap/tugasakhir/sipayroll/controller/LemburController.java
@@ -33,31 +33,6 @@ public class LemburController {
         return "form-add-lembur";
     }
 
-//    @PostMapping("/lembur/add")
-//    public String addLemburSubmit(@ModelAttribute LemburModel lembur, RedirectAttributes redir){
-//        UserModel user = userService.findUserByUsername(
-//                SecurityContextHolder.getContext().getAuthentication().getName());
-//        GajiModel gaji = user.getGaji();
-//        if(lemburService.bandingTanggal(lembur.getWaktuMulai(), lembur.getWaktuSelesai()) == true && lemburService.bandingJam(lembur.getWaktuMulai(), lembur.getWaktuSelesai()) == true){
-//            lembur.setStatusPersetujuan(0);
-//            lembur.setGaji(gaji);
-//            lembur.setKompensasiPerJam(120000);
-//            lemburService.addLembur(lembur);
-//            redir.addFlashAttribute("lembur", lembur);
-//            redir.addFlashAttribute("berhasil", "Penambahan lembur berhasil!");
-//            return "redirect:/lembur/add";
-//        } else if(lemburService.bandingTanggal(lembur.getWaktuMulai(), lembur.getWaktuSelesai()) == false){
-//            redir.addFlashAttribute("gagal", "Tanggal yang diberikan berbeda!");
-//            return "redirect:/lembur/add";
-//        } else if(lemburService.bandingJam(lembur.getWaktuMulai(), lembur.getWaktuSelesai()) == false){
-//            redir.addFlashAttribute("gagal", "Waktu mulai dan selesai tidak sesuai!");
-//            return "redirect:/lembur/add";
-//        } else{
-//            redir.addFlashAttribute("gagal", "ID Gaji belum terdaftar! Penambahan lembur gagal!");
-//            return "redirect:/lembur/add";
-//        }
-//    }
-
     @PostMapping("/lembur/add")
     public String addLemburSubmit(@ModelAttribute LemburModel lembur, RedirectAttributes redir){
         UserModel user = userService.findUserByUsername(
@@ -82,39 +57,28 @@ public class LemburController {
             return "redirect:/lembur/add";
         }
     }
-        // try{
-        //     GajiModel gaji = user.getGaji();
-        //     if(lemburService.bandingTanggal(lembur.getWaktuMulai(), lembur.getWaktuSelesai()) == true){
-        //         lembur.setStatusPersetujuan(0);
-        //         lembur.setGaji(gaji);
-        //         lembur.setKompensasiPerJam(120000);
-        //         lemburService.addLembur(lembur);
-        //         redir.addFlashAttribute("lembur", lembur);
-        //         redir.addFlashAttribute("berhasil", "Penambahan lembur berhasil!");
-        //         return "redirect:/lembur/add";
-        //     } else{
-        //         redir.addFlashAttribute("gagal", "Tanggal yang diberikan berbeda!");
-        //         return "redirect:/lembur/add";
-        //     }
-        // } catch(Exception e){
-        //     redir.addFlashAttribute("gagal", "ID Gaji belum terdaftar! Penambahan lembur gagal!");
-        //     return "redirect:/lembur/add";
-
-        // }
 
     @GetMapping("/lembur/ubah/{id}")
     public String ubahLemburFormPage(@PathVariable Integer id, Model model){
         LemburModel lembur = lemburService.getLemburById(id);
         model.addAttribute("lembur", lembur);
         model.addAttribute("dateTime", LocalDateTime.now());
-        return "form-ubah-lembur";
+        if(lembur.getStatusPersetujuan() == 2){
+            return "form-ubah-lembur2";
+        } else {
+            return "form-ubah-lembur";
+        }
     }
 
     @PostMapping("/lembur/ubah")
     public String ubahLemburFormSubmit(@ModelAttribute LemburModel lembur, RedirectAttributes redir){
         LemburModel lemburUpdated = lemburService.updateLembur(lembur);
         redir.addFlashAttribute("lembur", lembur);
-        redir.addFlashAttribute("berhasil", "Lembur berhasil diubah!");
+        if(lembur.getStatusPersetujuan() == 2){
+            redir.addFlashAttribute("berhasil", "Lembur sudah disetujui! Tidak dapat dilakukan perubahan lagi.");
+        } else {
+            redir.addFlashAttribute("berhasil", "Lembur berhasil diubah!");
+        }
         return "redirect:/lembur/ubah/"+lembur.getId();
     }
 
